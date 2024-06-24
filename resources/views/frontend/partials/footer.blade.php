e7aa4ba04281c6776a36dcf4859e5f3a890b987b
LAPTOP-E2U5TP4H\ASUS <gilbranbambimboo@gmail.com>
2024-06-24 18:46:54 +0700

    txt-decoration-none

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -5,9 +5,9 @@
       <div class="row">
         <div class="col-sm-12">
           <div class="copyright-box">
-            <p class="copyright">&copy; Copyright <strong><a href="https://instagram.com/gilbran_id">GilbranID</a></strong>. All Rights Reserved</p>
+            <p class="copyright">&copy; Copyright <strong><a class="text-decoration-none text-dark" href="https://instagram.com/gilbran_id">GilbranID</a></strong>. All Rights Reserved</p>
             <div class="credits">
-              Designed by <a href="https://instagram.com/gilbran_id">gilbranid</a><br>
+              Designed by <a class="text-decoration-none" href="https://instagram.com/gilbran_id">gilbranid</a><br>
             </div>
           </div>
         </div>
